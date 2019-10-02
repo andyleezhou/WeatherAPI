f8425332641219135968635557dacf9f6d5ee44e
Andy Zhou <andy.lee.zhou@gmail.com>
2019-10-02 16:14:54 -0700

    Updates

---
+++ precache-manifest.fe18ec1cd074775b8b4721af4c04d77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbe07d162b9ab70af0b78999f2c86c0",
+    "url": "/WeatherAPI/index.html"
+  },
+  {
+    "revision": "8eea4bf43d497812b7f3",
+    "url": "/WeatherAPI/static/css/main.2d841766.chunk.css"
+  },
+  {
+    "revision": "852eb091cfbd53e68267",
+    "url": "/WeatherAPI/static/js/2.501beb2b.chunk.js"
+  },
+  {
+    "revision": "8eea4bf43d497812b7f3",
+    "url": "/WeatherAPI/static/js/main.d3cda61a.chunk.js"
+  },
+  {
+    "revision": "9961d0ed471310e16636",
+    "url": "/WeatherAPI/static/js/runtime~main.8aabf327.js"
+  }
+]);
